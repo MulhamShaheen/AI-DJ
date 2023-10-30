7a640123e6a3abe4f06b1fd3a2c414373820d947
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2023-10-30 15:29:26 +0300

    add image resizing

--- infrastructure/classification/models.py
+++ infrastructure/classification/models.py
@@ -7,7 +7,7 @@ from torchvision.transforms import functional as F
 from typing import List
 from dataclasses import dataclass
 import logging
-from classes import Activity
+from .classes import Activity
 from torchvision import datasets, models, transforms
 from torch import nn
 import numpy as np
@@ -25,14 +25,6 @@ class Classifier(ABC):
     def __init__(self):
         pass
 
-    @abstractmethod
-    def detect_contours(self, image: np.ndarray) -> ClassifierPrediction:
-        """
-        Return boxes of all detected contours from image.
-        :param image: np.ndarray RGB image
-        :return: Activity
-        """
-        pass
 
 
 class DummyClassifier(Classifier):
@@ -49,21 +41,21 @@ class DummyClassifier(Classifier):
 class ResnetDetector(DummyClassifier):
     def __init__(self, detection_model_path: str):
         super().__init__(detection_model_path)
-        self._load_model()
         self._device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self._load_model()
 
-    def _load_model(self, trained_model_path="trained/model.pt"):
+    def _load_model(self, ):
         self.model = models.resnet18(weights=None)
         num_ftrs = self.model.fc.in_features
         self.model.fc = nn.Linear(num_ftrs, 15)
         self.model = self.model.to(self._device)
-        self.model.load_state_dict(torch.load(trained_model_path))
+        self.model.load_state_dict(torch.load(self.model_path))
 
     def predict_class(self, img_path: str):
         img = Image.open(img_path)
         img = F.pil_to_tensor(img)
-        transformer = transforms.Resize([3, 224, 224])
-        img = transformer(img).to(self._device)
-        pred = self.model(img)
+        transformer = transforms.Resize(244)
+        img = transformer(img).to(self._device, dtype=torch.float32)
+        pred = self.model(torch.unsqueeze(img, dim=0))
         result = torch.max(pred, 1)
         return result
