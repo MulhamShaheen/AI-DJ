5c041e49080c2bab27442ce7f85a1f839c286cfb
BunnyNoBugs <eugene200020@gmail.com>
2023-12-19 19:43:17 +0300

    fix model load

--- backend/AI_DJ/songs/infrastructure/pipelines.py
+++ backend/AI_DJ/songs/infrastructure/pipelines.py
@@ -48,10 +48,7 @@ class TextDummyPipline:
 
 
 class TextFaissPipeline:
-    def __init__(self, prompt: str, model_checkpoint: str = 'setu4993/LEALLA-small'):
-        self.prompt = prompt
-
-        # todo: shouldn't the model be loaded only once?
+    def __init__(self, model_checkpoint: str = 'setu4993/LEALLA-small'):
         # load model
         if 'LEALLA' in model_checkpoint:
             self.tokenizer = BertTokenizerFast.from_pretrained(model_checkpoint)
@@ -59,8 +56,7 @@ class TextFaissPipeline:
             self.model = self.model.eval()
 
         df_lyrics = pd.read_csv(
-            'https://raw.githubusercontent.com/MulhamShaheen/AI-DJ/main/prototype/NLP/data/lyrics.csv')
-        song_lyrics = df_lyrics['lyrics'].tolist()
+            'https://raw.githubusercontent.com/MulhamShaheen/AI-DJ/dev/search/scraper/songs_database_mini.csv')
         lyrics_dataset = Dataset.from_pandas(df_lyrics)
         lyrics_dataset = lyrics_dataset.map(lambda x: {'lyrics_embeddings': self._get_embeddings(x['lyrics'])[0]})
         lyrics_dataset.add_faiss_index(column='lyrics_embeddings')
@@ -80,9 +76,8 @@ class TextFaissPipeline:
         else:
             return embeddings
 
-    def predict_list(self, top_k=10):
+    def predict_list(self, prompt: str, top_k=10):
         scores, samples = self.lyrics_dataset.get_nearest_examples('lyrics_embeddings',
                                                                    self._get_embeddings(
-                                                                       [self.prompt]).cpu().detach().numpy(),
-                                                                   k=top_k)
+                                                                       [prompt]).cpu().detach().numpy(), k=top_k)
         return scores, samples
